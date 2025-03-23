f82b87c6543de7ca4cfe871c010aa0bddfb40fb9
nathsdm <nathan.soares_de_melo@telecom-sudparis.eu>
2025-03-23 02:05:43 +0100

    Added preload for gifs

--- src/pages/Other.tsx
+++ src/pages/Other.tsx
@@ -1,8 +1,8 @@
 const Other = () => {
   return (
     <div>
-      <img src="/oies.gif" alt="Animated GIF" />
-      <img src="/samantha-olaf.gif" alt="Animated GIF" />
+      <img rel="preload" src="/oies.gif" alt="Animated GIF" />
+      <img rel="preload" src="/samantha-olaf.gif" alt="Animated GIF" />
     </div>
   );
 };
